b2bd3419651a0b9cafeda06446e8de1b9c81af07
jay <rameshpvb@gmail.com>
2023-10-19 02:02:35 +0800

    feat: complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(phrase) {
+  split_phrase = phrase.split("");
+  split_phrase = split_phrase.reverse();
+  reversed_phrase = split_phrase.join("");
+  return reversed_phrase
 };
 
 // Do not edit below this line
