92297ffb53811dc53a02b90b461d23c76c54165f
jay <rameshpvb@gmail.com>
2023-10-18 17:41:26 +0800

    feat: complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(phrase, repeats) {
+  finalString = '';
+  if (repeats >= 0) {
+    for (let i=0; i<repeats; i++) {
+      finalString += phrase;
+    }
+  }
+  else {
+    finalString = "ERROR";
+  }
+  return finalString;
 };
 
 // Do not edit below this line
