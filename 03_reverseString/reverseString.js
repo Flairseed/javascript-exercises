1449613ffdf29bc7061365220eb3a7f4b0dd2aa7
jay <rameshpvb@gmail.com>
2023-10-19 03:02:36 +0800

    style: add missing ;

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -2,7 +2,7 @@ const reverseString = function(phrase) {
   let split_phrase = phrase.split("");
   split_phrase = split_phrase.reverse();
    const reversed_phrase = split_phrase.join("");
-  return reversed_phrase
+  return reversed_phrase;
 };
 
 // Do not edit below this line
