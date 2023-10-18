c337c0a40ee118fac63aeb2fa046e51ad096ecff
jay <rameshpvb@gmail.com>
2023-10-19 02:45:27 +0800

    feat: complete exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+  let sum = 0;
+  const lowerBound = num1 < num2 ? num1 : num2;
+  const upperBound = num1 > num2 ? num1 : num2;
+  if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+  for (let i = lowerBound; i <= upperBound; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
