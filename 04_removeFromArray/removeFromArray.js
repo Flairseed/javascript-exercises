56948af63f90515584350d5dfd02abfccf42fb92
jay <rameshpvb@gmail.com>
2023-10-19 02:20:17 +0800

    feat: complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...removeElements) {
+  filterFunction = (element) => !removeElements.includes(element);
+  inputArray = inputArray.filter(filterFunction);
+  return inputArray;
 };
 
 // Do not edit below this line
