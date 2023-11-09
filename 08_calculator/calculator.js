f0146b1e16a5df14e4cfe4aa256acef425411d56
jay <rameshpvb@gmail.com>
2023-11-09 18:37:48 +0800

    feat: complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+	let total = 0;
+  for (let num of nums) {
+    total += num;
+  }
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let total = nums.length > 0 ? 1 : 0;
+  for (let num of nums) {
+    total *= num;
+  }
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1
+	for (i = num; i > 1; i--) {
+    total *= i
+  }
+  return total;
 };
 
 // Do not edit below this line
