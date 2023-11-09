dc8e6a72e456c3dd39d3fb78ac5c2045d45b5a01
jay <rameshpvb@gmail.com>
2023-11-09 19:00:06 +0800

    feat: complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+  const originalWord = word.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  const reverseWord = Array.from(originalWord).reverse().join("");
+  return originalWord === reverseWord;
 };
 
 // Do not edit below this line
