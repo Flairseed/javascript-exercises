6c5a01509ce930430e37e7bfcb422c796b28ae94
jay <rameshpvb@gmail.com>
2023-11-09 21:59:07 +0800

    feat: complete exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  const sortedPeople = people.sort((a, b) => {
+    for (i of [a, b]) {
+      if (!("yearOfDeath" in i)) {
+        i.yearOfDeath = new Date().getFullYear();
+      }
+    }
+    ageA = a.yearOfDeath - a.yearOfBirth;
+    ageB = b.yearOfDeath - b.yearOfBirth;
+    return ageA > ageB ? -1 : 1;
+  });
+  return sortedPeople[0];
 };
 
 // Do not edit below this line
